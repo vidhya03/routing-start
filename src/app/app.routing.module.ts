e62ea6349f5ea05894d22905133dcd080a7a50f9
Vidhyadharan D <it.vidhyadharan@gmail.com>
2018-08-06 13:17:20 +0530

    added routing

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -8,6 +8,8 @@ import { ServerComponent } from './servers/server/server.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AuthGuard } from './auth.guard.service';
+import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard.service';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -23,12 +25,18 @@ const appRoutes: Routes = [
     component: ServersComponent,
     children: [
       { path: ':id', component: ServerComponent },
-      { path: ':id/edit', component: EditServerComponent }
+      {
+        path: ':id/edit',
+        component: EditServerComponent,
+        canDeactivate: [CanDeactivateGuard]
+      }
     ]
   },
+  // { path: 'page-not-found', component: PageNotFoundComponent },
   {
     path: 'page-not-found',
-    component: PageNotFoundComponent
+    component: ErrorPageComponent,
+    data: { message: 'page not found' }
   },
   {
     path: '**',
